dd6a586e6ccc0828fd0b4f667b18f6bb306abcd1
lchen104 <lchen@zdmedia.com>
2023-06-23 11:29:45 -0400

    Found the pokemon type and set it to coinciding color in the colors object

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ const colors = {
 	normal: '#F5F5F5'
 }
 
+const main_types = Object.keys(colors);
+console.log(colors);
+
+
 const fetchPokemons = async () => {
     for(let i = 1; i <= pokemon_count; i++) {
-        console.log(i);
+        // console.log(i);
         await getPokemon(i);
     }
 }
@@ -30,10 +34,42 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    createPokemonCard(data);
 }
 
 
-const create
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
 
-// fetchPokemons();
+// pokemon types
+const poke_types = pokemon.types.map(type => type.type.name);
+// console.log(poke_types);
+
+const type = main_types.find(type => poke_types.indexOf(type) > -1)
+// console.log(type);
+
+const color = colors[type];
+// console.log(color);
+
+pokemonEl.style.background = color;
+
+    // card
+    const pokemonInnerHTML = `
+    <div class="image-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+    </div>
+
+    <div class="info">
+        <span class="number">#${pokemon.id}</span>
+        <h3 class="name">${pokemon.name}</h3>
+        <small class="type">Type <span>${pokemon.type}</span></small>
+    </div>
+    `;
+
+    pokemonEl.innerHTML = pokemonInnerHTML;
+    poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();
